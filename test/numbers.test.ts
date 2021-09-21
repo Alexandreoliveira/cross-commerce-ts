d1c3b7c3cc9c9d49c241133d741cff6b4824dc5c
Alexandre Meireles <alexandre1.95oliveira@gmail.com>
2021-09-21 13:53:16 -0300

    implementado testes da API disponibilizada, teste de status 200 e teste da ordenação dos números

--- test/numbers.test.ts
+++ test/numbers.test.ts
@@ -1,3 +1,32 @@
 import * as axios from 'axios';
-import { Numbers } from '../src/database/entity/Numbers';
-import { Core } from '../src/sys/core';
+
+jest.setTimeout(300000)
+
+let numbers_all: number[];
+
+test('Teste resposta da API', () => {
+
+    return axios.default
+        .get('http://162.214.193.97:5555/api/v1/numbers')
+        .then((res) => {
+            numbers_all = res.data.numbers;
+            expect(res.status).toEqual(200)
+        })
+        .catch(function (error) {
+            fail(error)
+        });
+})
+
+test('Ordenação dos Números', () => {
+    let correto = true
+    let num = -9999;
+    numbers_all.forEach((e) => {
+
+        if (e > num) {
+            num = e;
+        } else {
+            correto = false;
+        }
+    })
+    expect(correto).toBeTruthy
+})
